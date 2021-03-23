6877691f1b17c6f4e44b1c20aac32a596cc67f9d
rajeshRoul <roul.rajesh28@gmail.com>
2021-03-23 18:53:31 +0530

    Third commit: Created a controller

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const homecontroller = require('../controllers/home_controller');
 
 console.log('routes loaded');
+
+router.get('/', homecontroller.home);
 
 module.exports = router;
