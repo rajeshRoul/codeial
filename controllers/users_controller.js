81f438ddb8b80d677ffbde100974747146b6ad39
rajeshRoul <roul.rajesh28@gmail.com>
2021-03-23 23:33:39 +0530

    Created posts route and controller under users route

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile = function(req, res){
     return res.end('<h1> Users Profile <h1>');
 };
+
+module.exports.posts = function(req, res){
+    return res.end('<h1>Users posts<h2>');
+}
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,6 @@ const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
 
+router.get('/posts', usersController.posts);
+
 module.exports = router;
