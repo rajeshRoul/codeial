c6856f7791c662a969f92da6de09d7a1c3d16be0
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-02 19:18:07 +0530

    Created forms for sign in and sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,16 +8,26 @@ module.exports.posts = function(req, res){
     return res.end('<h1>Users posts<h2>');
 }
 
-// User Sign in
+// Render Sign in page
 module.exports.signIn = function(req, res){
     return res.render('user_sign_in', {
         title : 'Codeial | Sign In'
     })
 }
 
-// User Sign up
+// Render Sign up page
 module.exports.signUp = function(req, res){
     return res.render('user_sign_up', {
         title : 'Codeial | Sign Up'
     })
+}
+
+// Get the sign up data
+module.exports.create = function(req, res){
+    // TODO later
+}
+
+// Sign in and create a session for user
+module.exports.createSession = function(req, res){
+    // TODO later
 }
