7fb8a766bb046d6ba2bfc94192ff8b84e99a076f
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-02 12:07:59 +0530

    Implemented partials for header and footer sections

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile = function(req, res){
-    return res.end('<h1> Users Profile <h1>');
+    return res.render('users_profile', {
+        title : 'profile'
+    });
 };
 
 module.exports.posts = function(req, res){
