d01138a4d0ca11459d6f1829caabd00079f6d973
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-02 12:36:25 +0530

    Setup common layout for all the pages

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 
 app.use('/', require('./routes/index'));
+
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
