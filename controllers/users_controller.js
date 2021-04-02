e9063bdddb2f13bdc42f88ba7441df22b0acffeb
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-02 21:32:56 +0530

    Added controller for sign up request

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('users_profile', {
         title : 'profile'
@@ -24,7 +26,21 @@ module.exports.signUp = function(req, res){
 
 // Get the sign up data
 module.exports.create = function(req, res){
-    // TODO later
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email : req.body.email}, function(err, user){
+        if(err){console.log('Error while finding user on sign up'); return;}
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in registering user'); return;}
+                res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    });
 }
 
 // Sign in and create a session for user
