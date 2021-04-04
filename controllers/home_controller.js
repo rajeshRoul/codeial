ba505d3a7ed3079aea4e8e776af4768fa64e158e
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-04 20:24:56 +0530

    Display comments for every post and used nested populating

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -9,7 +9,15 @@ module.exports.home = function(req, res){
     // });
     // });
 
-    Post.find({}).populate('user').exec(function(err, posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         if(err){console.log('Error in finding posts'); return;}
         return res.render('home', {
             title : 'Codeial | Home',
