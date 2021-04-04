4c7fada58db29a1bcfa53c973022af6846ce3ee6
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-04 19:04:35 +0530

    Display posts and related user on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,19 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    return res.render('home', {
-        title : 'home'
+    // Post.find({}, function(err, posts){
+    //     if(err){console.log('Error in finding posts'); return;}
+    //     return res.render('home', {
+    //     title : 'Codeial | Home',
+    //     posts : posts
+    // });
+    // });
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        if(err){console.log('Error in finding posts'); return;}
+        return res.render('home', {
+            title : 'Codeial | Home',
+            posts : posts
+        });
     });
 };
