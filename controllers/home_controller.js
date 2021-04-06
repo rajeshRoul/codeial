78782157f658cb909c3c0d4263f5d6fe62a24e96
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-06 16:33:06 +0530

    Added user profile links on home page and added feature to update user details in the database

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports.home = function(req, res){
     // Post.find({}, function(err, posts){
@@ -19,9 +20,14 @@ module.exports.home = function(req, res){
     })
     .exec(function(err, posts){
         if(err){console.log('Error in finding posts'); return;}
-        return res.render('home', {
-            title : 'Codeial | Home',
-            posts : posts
+
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title : 'Codeial | Home',
+                posts : posts,
+                all_users : users
+            });
         });
+        
     });
 };
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,25 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
-    return res.render('users_profile', {
-        title : 'profile'
+    User.findById(req.params.id, function(err, user){
+        return res.render('users_profile', {
+            title : 'profile',
+            profile_user: user
+        });
     });
+    
 };
+
+module.exports.update = function (req, res) {
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            if(err){console.log('Error in updating user details'); return;}
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+}
 
 module.exports.posts = function(req, res){
     return res.end('<h1>Users posts<h2>');
