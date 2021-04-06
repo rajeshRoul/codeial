6010e936fc4dd5e363aee6fede90b1748a2dbd2f
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-06 18:04:34 +0530

    Converted code in home_controller to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,33 +1,26 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req, res){
-    // Post.find({}, function(err, posts){
-    //     if(err){console.log('Error in finding posts'); return;}
-    //     return res.render('home', {
-    //     title : 'Codeial | Home',
-    //     posts : posts
-    // });
-    // });
+module.exports.home = async function(req, res){
 
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        if(err){console.log('Error in finding posts'); return;}
-
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title : 'Codeial | Home',
-                posts : posts,
-                all_users : users
-            });
+    try{
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
         });
-        
-    });
+        let users = await User.find({});
+    
+        return res.render('home', {
+            title : 'Codeial | Home',
+            posts : posts,
+            all_users : users
+        });
+    }catch(err){
+        console.log("Error", err);
+        return;
+    }
 };
