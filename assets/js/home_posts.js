34ee8bd91a3ddb9859454fd6bcd7d7f041368b48
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-07 23:20:08 +0530

    Converting to ajax -- Creating a Post::Receiving Data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,7 +11,8 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: function(data){
-                    console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
                 },error: function(error){
                     console.log(error.responseText);
                 }
@@ -20,6 +21,28 @@
     }
 
     // Method to create a post in dom
+    let newPostDom = function(post){
+        return $(`<li id="post-${post._id}">
+            <small>
+                <a class="delete-post-button" href="/posts/destroy/${ post.id }"> X </a>
+            </small>
+            <p> ${ post.content }</p>
+            <small> ${ post.user.name } </small>
+            <div class="post-comments">
+                    <form action="/comments/create" method="POST">
+                        <input type="text" name="content" placeholder="Type here to add comment..." required>
+                        <input type="hidden" name="post" value="${ post._id }">
+                        <input type="submit" value="Add Comment">
+                    </form>    
+        
+                <div class="post-comments-list">
+                    <ul id="post-comments-${ post._id }">
+        
+                    </ul>
+                </div>
+            </div>
+        </li>`)
+    }
 
     createPost();
 }
