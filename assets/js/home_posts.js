a63b4911cf434d5e4181671b1879d607221bd3d8
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-08 13:06:16 +0530

    Converted Deleting a Post to AJAX

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -13,6 +13,7 @@
                 success: function(data){
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
                 },error: function(error){
                     console.log(error.responseText);
                 }
@@ -24,7 +25,7 @@
     let newPostDom = function(post){
         return $(`<li id="post-${post._id}">
             <small>
-                <a class="delete-post-button" href="/posts/destroy/${ post.id }"> X </a>
+                <a class="delete-post-button" href="/posts/destroy/${ post._id }"> X </a>
             </small>
             <p> ${ post.content }</p>
             <small> ${ post.user.name } </small>
@@ -41,8 +42,26 @@
                     </ul>
                 </div>
             </div>
-        </li>`)
+        </li>`);
+    };
+
+    // method to delete a post from dom
+    let deletePost = function(deleteLink){
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data){
+                    $(`#post-${data.data.post_id}`).remove();
+                },error: function(err){
+                    console.log(err.responseText);
+                }
+            })
+        })
     }
+
 
     createPost();
 }
