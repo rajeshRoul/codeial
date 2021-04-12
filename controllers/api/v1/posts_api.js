e4c8e44405e6f967597974fed331289df5374ccd
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-12 20:21:15 +0530

    Setup API to send posts to the user

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,24 @@
-module.exports.index = function(req, res){
+const Post = require('../../../models/post');
+
+module.exports.index = async function(req, res){
+
+    let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            options: {
+                sort: {
+                    'createdAt': -1
+                }
+            },
+            populate: {
+                path: 'user'
+            }
+        });
+
     return res.json(200, {
         message: "List of posts",
-        posts: []
+        posts: posts
     });
 };
