66f02facb61aa1ecf3ae4a78a84945b0fe0edffb
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-12 20:36:07 +0530

    Setup API to delete posts from database

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,4 +1,5 @@
 const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
 
 module.exports.index = async function(req, res){
 
@@ -22,3 +23,30 @@ module.exports.index = async function(req, res){
         posts: posts
     });
 };
+
+module.exports.destroy = async function(req, res){
+    try{
+        let post = await Post.findById(req.params.id);
+
+        // if(post.user == req.user.id){
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+            
+            // req.flash('success', 'Post and associated comments deleted');
+            return res.json(200, {
+                message: "Post and associated comments deleted successfully"
+            });
+        // }else{
+        //     req.flash('error', 'You cannot delete this post');
+        //     return res.redirect('back');
+        // }
+    }catch(err){
+        // req.flash('error', err);
+        console.log("****Error:", err);
+        return res.json(500, {
+            message: "Internal Server Error"
+        });
+    }
+    
+}
