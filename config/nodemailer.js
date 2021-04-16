eb71325280aa7d913f1418487a97fcf5c73680a5
rajeshRoul <roul.rajesh28@gmail.com>
2021-04-16 19:27:40 +0530

    Send Html template emails

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -20,7 +20,7 @@ let renderTemplate = (data, relativePath)=>{
         path.join(__dirname, '../views/mailers', relativePath),
         data,
         function(err, template){
-            if(err){console.log('error in rendering template'); return;}
+            if(err){console.log('error in rendering template', err); return;}
             mailHTML = template;
         }
     )
--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,12 +1,12 @@
 const nodemailer = require('../config/nodemailer');
 
 exports.newComment = (comment) => {
-    console.log('Inside new comment mailer');
+    let htmlString = nodemailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
     nodemailer.transporter.sendMail({
         from: 'roul.rajesh28@gmail.com',
         to: comment.user.email,
         subject: "New Comment Published",
-        html: '<h1>Hey, your comment is now published<h1>'
+        html: htmlString
     }, (err, info)=> {
         if(err){console.log('Error in sending mail: ', err); return;}
 
